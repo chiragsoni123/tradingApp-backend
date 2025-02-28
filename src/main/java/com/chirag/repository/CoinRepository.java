f6da66f98743dfc97856d499cfad975d7df5680c
Chirag <chiragsoni8857@gmail.com>
2025-02-28 23:58:43 +0530

    integrated coin API

---
+++ src/main/java/com/chirag/repository/CoinRepository.java
@@ -0,0 +1,7 @@
+package com.chirag.repository;
+
+import com.chirag.modal.Coin;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CoinRepository extends JpaRepository<Coin, String> {
+}
